4395707f050cee5153be44ce118bd028076a71de
Navid Khan <navidkhan@Navids-MacBook-Pro.local>
2023-02-16 12:27:30 -0500

    Added tags and environment variables

--- variables.tf
+++ variables.tf
@@ -1,5 +1,17 @@
+locals {
+  common_tags = {
+    Environment = var.env
+    Billing     = "DevOps-Toolkit"
+    Program     = "CCP"
+  }
+}
 
 variable "location" {
   type    = string
   default = "East Asia"
+}
+
+variable "env" {
+  type    = string
+  description = "An environment we work within"
 }
